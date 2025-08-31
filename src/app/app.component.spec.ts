0fcd061148a09edd4b800ee9103d78d4ce7cbba5
tal931 <t28277189@gmail.com>
2025-08-31 12:07:27 +0300

    rename the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'work-logix-new-client' title`, () => {
+  it(`should have the 'projector-log-website' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   });
@@ -23,6 +23,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, work-logix-new-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, projector-log-website');
   });
 });
