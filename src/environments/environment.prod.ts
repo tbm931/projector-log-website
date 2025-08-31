1a762c16f02eaaa615ece30187b415f55b77c34c
tal931 <t28277189@gmail.com>
2025-08-31 12:01:15 +0300

    fixing compilation error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,10 @@ import type { Environment } from './enviroments.interface';
 export const environment: Environment = {
   production: true,
   apiBaseUrl: 'https://log.dev.ysprojector.co.il/api',
-  translationVersion
+  translationVersion,
+  //צריך להחליף לקישור חי
+  paymentLinks: {
+    plan_100: 'https://buy.stripe.com/test_14k3fS4bK4bE7TG5kl',
+    plan_250: 'https://buy.stripe.com/test_9AQdRk4bK4bE7TG5kl'
+  }
 };
