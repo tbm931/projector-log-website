a99486163088bc78d1708b431183b25e0c0e5e53
tal931 <t28277189@gmail.com>
2025-08-31 11:07:26 +0300

    create components

---
+++ src/app/features/thank-you/thank-you.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-you',
+  imports: [],
+  templateUrl: './thank-you.component.html',
+  styleUrl: './thank-you.component.scss'
+})
+export class ThankYouComponent {
+
+}
